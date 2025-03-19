d7de6f3cd14fbd07b80452458dfb2135fcee99df
mariish <mariish@uio.no>
2025-03-19 12:56:47 +0100

    Lagd DataSource for HvaKosterStrømmen, den er uferdig

--- app/src/main/java/in2000/team42/data/HvaKosterStrømmen/HvaKosterStrømmenDataSource.kt
+++ app/src/main/java/in2000/team42/data/HvaKosterStrømmen/HvaKosterStrømmenDataSource.kt
@@ -1,10 +1,36 @@
 package in2000.team42.data.HvaKosterStrømmen
 
+import android.util.Log
+import in2000.team42.model.hvaKosterStrømmen.HvaKosterStrømmen
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.cio.CIO
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.get
+
 class HvaKosterStrømmenDataSource {
 
-    private val client=HttpClient{
-        install(Conten)
+    private val ktorHttpclient=HttpClient(CIO){
+        install(ContentNegotiation){
+            json()
+        }
     }
+
+//
+//    private val baseUrl="https://www.hvakosterstrommen.no/api/v1/prices/$year/$month-$day_$area.json"
+//
+//    suspend fun getStrømInfo(year:Int,month:Int,day:Int,area:String):List<HvaKosterStrømmen>{
+//
+//        return try{
+//            val response:List<HvaKosterStrømmen> = ktorHttpclient.get(baseUrl).body()
+//            response
+//        }
+//        catch(e:Exception) {
+//            Log.d("HVAKOSTERSTRØMMENDATASOURCE","Feil ved henting av strøminfo:${e.message}")
+//            emptyList()
+//        }
+//    }
 
 
 

8951a1b41d4a083f738bcec50dcc7346836af09d
Ekram Aly <ekramta@uio.no>
2025-03-23 10:39:44 +0100

    repo

--- app/src/main/java/in2000/team42/data/HvaKosterStrømmen/HvaKosterStrømmenRepo.kt
+++ app/src/main/java/in2000/team42/data/HvaKosterStrømmen/HvaKosterStrømmenRepo.kt
@@ -1,17 +1,26 @@
 package in2000.team42.data.HvaKosterStrømmen
 
+import android.util.Log
 import in2000.team42.model.hvaKosterStrømmen.HvaKosterStrømmen
 
 class HvaKosterStrømmenRepo {
 
-    private val strømmenDataSource=HvaKosterStrømmenDataSource()
+    private val dataSource = HvaKosterStrømmenDataSource()
 
 
-    suspend fun getStromPriser(year :Int, month : Int, day : Int, area: String = toString()):List<HvaKosterStrømmen>{
-       return strømmenDataSource.getStromInfo(year = year, month = month, day = day, area = area)
-
+    suspend fun getStrømPriser(year :Int, month : Int, day : Int, area: String ): Result<List<HvaKosterStrømmen>>{
+      return try {
+          val prices = dataSource.getStromInfo(year, month, day, area)
+          if (prices.isNotEmpty()){
+              Log.d("REPOSITORY", "Hentet ${prices.size} priser for $year-$month-$day i $area")
+              Result.success(prices)
+          } else {
+              Log.d("REPOSITORY", "Ingen priser hentet for $year-$month-$day i $area")
+              Result.failure(Exception("Ingen data tilgjengelig"))
+          }
+      } catch (e: Exception) {
+          Log.e("REPOSITORY", "Feil ved henting av strømpriser: ${e.message}")
+          Result.failure(e)
+      }
     }
-
-
-
 }

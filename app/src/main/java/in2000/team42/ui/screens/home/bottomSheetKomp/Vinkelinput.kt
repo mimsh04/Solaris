4c642ab05fcb472775f5c1f9a87ff265c99f9d53
Sindre Denby <sidenby1@gmail.com>
2025-03-25 14:08:19 +0100

    ferdig med api

--- app/src/main/java/in2000/team42/ui/screens/home/bottomSheetKomp/Vinkelinput.kt
+++ app/src/main/java/in2000/team42/ui/screens/home/bottomSheetKomp/Vinkelinput.kt
@@ -4,18 +4,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Build
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 
--- app/src/main/java/in2000/team42/ui/screens/home/bottomSheetKomp/Vinkelinputs.kt
+++ app/src/main/java/in2000/team42/ui/screens/home/bottomSheetKomp/Vinkelinputs.kt
@@ -1,17 +1,10 @@
 package in2000.team42.ui.screens.home.bottomSheetKomp
-
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Build
-import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun Vinkelinputs(

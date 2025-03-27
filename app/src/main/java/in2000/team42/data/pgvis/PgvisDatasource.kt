3b4e5a9aae55f43be77f881ae7b581ef262a1774
Sindre Denby <sidenby1@gmail.com>
2025-03-27 16:26:15 +0100

    lagt til requesten

--- app/src/main/java/in2000/team42/data/pgvis/PgvisDatasource.kt
+++ app/src/main/java/in2000/team42/data/pgvis/PgvisDatasource.kt
@@ -1,6 +1,7 @@
 package in2000.team42.data.pgvis
 
 import in2000.team42.data.pgvis.model.DailyProfile
+import in2000.team42.data.pgvis.model.KwhMonthlyResponse
 import in2000.team42.data.pgvis.model.RadiationResponse
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
@@ -12,7 +13,11 @@ import io.ktor.client.request.get
 class PgvisDatasource {
     private val ktorHttpClient = HttpClient(CIO) {
         install(ContentNegotiation) {
-            json()
+            json (
+                kotlinx.serialization.json.Json {
+                    ignoreUnknownKeys = true
+                }
+            )
         }
     }
 
@@ -38,6 +43,43 @@ class PgvisDatasource {
         return try {
             val response: RadiationResponse = ktorHttpClient.get(url).body()
             response.outputs?.daily_profile ?: emptyList()
+        } catch (e: Exception) {
+            println("Error fetching radiation data: ${e.message}")
+            emptyList()
+        }
+    }
+
+    enum class PvTech(val value: String) {
+        CRYST_SI("crystSi"),
+        CIS("CIS"),
+        CD_TE("CdTe"),
+        UNKNOWN("Unknown")
+    }
+
+
+    // https://re.jrc.ec.europa.eu/api/v5_3/PVcalc?outputformat=json&lat=59.668&lon=10.789&raddatabase=PVGIS-SARAH3&peakpower=2&loss=14&mountingplace=building&pvtechchoice=crystSi&angle=35&aspect=0&js=1
+    suspend fun getMonnthlyKwh(
+        lat: Double,
+        lon: Double,
+        incline: Float,
+        retning: Float = 0f,
+        peakPower: Float,
+        pvTech: PvTech,
+
+    ) :List<KwhMonthlyResponse.MonthlyKwhData> {
+        val url = "https://re.jrc.ec.europa.eu/api/v5_3/PVcalc?outputformat=json" +
+                "&lat=$lat" +
+                "&lon=$lon" +
+                "&raddatabase=PVGIS-SARAH3" +
+                "&peakpower=$peakPower" +
+                "&loss=14" +
+                "&mountingplace=building" +
+                "&pvtechchoice=${pvTech.value}" +
+                "&angle=$incline" +
+                "&aspect=$retning"
+        return try {
+            val response = ktorHttpClient.get(url).body<KwhMonthlyResponse.Response>()
+            response.outputs.monthly.fixed.monthly_kwh
         } catch (e: Exception) {
             println("Error fetching radiation data: ${e.message}")
             emptyList()
---
+++ app/src/main/java/in2000/team42/data/pgvis/model/KwhMonthlyResponse.kt
@@ -0,0 +1,37 @@
+package in2000.team42.data.pgvis.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+abstract class KwhMonthlyResponse {
+    @Serializable
+    data class Response(
+        val outputs: Outputs
+    )
+
+    @Serializable
+    data class Outputs(
+        val monthly: Monthly
+    )
+
+    @Serializable
+    data class Monthly(
+        val fixed: Fixed
+    )
+
+    @Serializable
+    data class Fixed(
+        val monthly_kwh: List<MonthlyKwhData>
+    )
+
+    @Serializable
+    data class MonthlyKwhData(
+        val month: Int,
+        @SerialName("E_d") val avarageDaily: Double,
+        @SerialName("E_m")val averageMonthly: Double,
+        @SerialName("H(i)_d") val averageDailyRadiation: Double,
+        @SerialName("H(i)_m") val averageMontlyRadiation: Double,
+        @SerialName("SD_m") val yearlyVariation: Double
+    )
+}
+

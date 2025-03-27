3ce2be46f4fceb1f3a308008f8cba443fed56679
Sindre Denby <sidenby1@gmail.com>
2025-03-27 12:54:19 +0100

    lagt til error farge ved inputs

--- app/src/main/java/in2000/team42/ui/screens/home/bottomSheet/Vinkelinput.kt
+++ app/src/main/java/in2000/team42/ui/screens/home/bottomSheet/Vinkelinput.kt
@@ -5,11 +5,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -22,6 +32,13 @@ fun Vinkelinput(
     range: IntRange = 0..90,
     icon: ImageVector
 ) {
+    var showedVal by remember { mutableStateOf(verdi.toInt().toString()) }
+
+    fun getOutlineColor(showedText: String): Color {
+        return if (showedText.isEmpty()) Color.Red
+        else if (showedText.toIntOrNull() in range) Color.Green
+        else Color.Red
+    }
 
     Row(
         modifier = Modifier
@@ -34,16 +51,25 @@ fun Vinkelinput(
             modifier = Modifier.padding(end = 12.dp)
         )
         OutlinedTextField(
-            value = verdi.toInt().toString(),
+            value = showedVal,
             onValueChange = { newValue ->
-                newValue.toIntOrNull()?.let {
-                    if (it in range) onValueChange(it.toFloat())
+                showedVal = newValue
+                newValue.toFloatOrNull()?.let {
+                    if (it.toInt() in range) {
+                        onValueChange(it)
+                    } else {
+                        onValueChange(0f)
+                    }
                 }
             },
             label = { Text(tittel) },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             singleLine = true,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = getOutlineColor(showedVal),
+                unfocusedBorderColor = getOutlineColor(showedVal)
+            )
         )
     }
 

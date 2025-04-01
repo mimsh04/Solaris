bf70af271cc61f20758f108dd75c67304f97384e
Sindre Denby <sidenby1@gmail.com>
2025-04-01 16:09:36 +0200

    Fiksa litt layout

--- app/src/main/java/in2000/team42/ui/screens/home/bottomSheet/Produksjon.kt
+++ app/src/main/java/in2000/team42/ui/screens/home/bottomSheet/Produksjon.kt
@@ -1,12 +1,18 @@
 package in2000.team42.ui.screens.home.bottomSheet
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import in2000.team42.data.pgvis.model.KwhMonthlyResponse
 import in2000.team42.ui.screens.home.HomeViewModel
 
@@ -23,16 +29,20 @@ fun Produksjon(viewModel: HomeViewModel) {
         return tot
     }
 
-    Row {
+    Row (modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+        ){
         if (kwhPerMonth.value.isEmpty()) {
             Text("Ingen data (velg posisjon med search)")
         } else {
-            Column {
+            Column  (
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
                 Text("Årlig produksjon: ${getYearlyProduction(kwhPerMonth.value)} kWh")
                 Button(
                     onClick = {
                         viewModel.updateAllApi()
-                    }
+                    },
                 ) {
                     Text("Hent på nytt")
                 }

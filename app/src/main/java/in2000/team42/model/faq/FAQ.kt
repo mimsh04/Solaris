89bf67b12ac416bc3663925c01d95bb62a6fefe8
Ekram Aly <ekramta@uio.no>
2025-04-02 17:45:19 +0200

    skrev noe Q/A

--- app/src/main/java/in2000/team42/model/faq/FAQ.kt
+++ app/src/main/java/in2000/team42/model/faq/FAQ.kt
@@ -1,7 +1,8 @@
 package in2000.team42.model.faq
 
-data class FAQ (val question: String, val answer: String){
-    val faqItems = listOf(
+data class FAQ (val question: String, val answer: String)
+
+val faqItems = listOf(
         FAQ("Hvorfor utvikles denne appen?",
             "Appen skal hjelpe brukere med å beregne den forventede strømproduksjonen" +
                     " fra solcellepaneler basert på lokale klimadata, takforhold og solcellevirkningsgrad."),
@@ -9,5 +10,4 @@ data class FAQ (val question: String, val answer: String){
             "Selv om man kanskje tror solceller fungerer best i varme klima," +
                     " er de mest effektive ved ca. -5°C og presterer dårlig ved temperaturer over 25°C." +
                     " I Sør-Norge kan solceller faktisk produsere mer energi enn i Afrika, så lenge det ikke er snødekke.")
-    )
-}
+)

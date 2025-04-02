cc724a6d4506972c062d549af9fcaba08617919a
Ekram Aly <ekramta@uio.no>
2025-04-02 16:07:14 +0200

    laga en data klasse for FAQ

---
+++ app/src/main/java/in2000/team42/data/faq/FAQ.kt
@@ -0,0 +1,13 @@
+package in2000.team42.data.faq
+
+data class FAQ (val question: String, val answer: String){
+    val faqItems = listOf(
+        FAQ("Hvorfor utvikles denne appen?",
+            "Appen skal hjelpe brukere med å beregne den forventede strømproduksjonen" +
+                    " fra solcellepaneler basert på lokale klimadata, takforhold og solcellevirkningsgrad."),
+        FAQ("Hvordan kan solceller fungere effektivt i Norge?",
+            "Selv om man kanskje tror solceller fungerer best i varme klima," +
+                    " er de mest effektive ved ca. -5°C og presterer dårlig ved temperaturer over 25°C." +
+                    " I Sør-Norge kan solceller faktisk produsere mer energi enn i Afrika, så lenge det ikke er snødekke.")
+    )
+}
--- app/src/main/java/in2000/team42/ui/screens/faq/FaqDialog.kt
+++ app/src/main/java/in2000/team42/ui/screens/faq/FaqDialog.kt
@@ -3,7 +3,6 @@ package in2000.team42.ui.screens.faq
 
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 

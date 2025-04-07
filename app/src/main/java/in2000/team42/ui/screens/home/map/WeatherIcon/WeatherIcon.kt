c72f5a1972dda845b4687086a7065b9d354ad979
Noah <niteigen@uio.no>
2025-04-07 12:15:34 +0200

    La til sol for ikon, må fikse bugs

--- app/src/main/java/in2000/team42/ui/screens/home/map/WeatherIcon/WeatherIcon.kt
+++ app/src/main/java/in2000/team42/ui/screens/home/map/WeatherIcon/WeatherIcon.kt
@@ -1,12 +1,18 @@
 package in2000.team42.ui.screens.home.map.WeatherIcon
 
+import in2000.team42.R
+import android.R.attr.contentDescription
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.*
 //import androidx.compose.material.icons.filled.Cloud
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Popup
@@ -18,18 +24,14 @@ fun WeatherIconButton(
     var showPopup by remember { mutableStateOf(false) }
 
     Box(modifier = modifier) {
-        TextButton(
+        IconButton(
             onClick = { showPopup = true }
         ) {
-            Text(
-                text = "test",
-                color = MaterialTheme.colorScheme.primary
-            )
-            /*Icon(
-                imageVector = Icons.Default.Cloud,
+            Icon(
+                painter = painterResource(id = R.drawable.sun_icon_in2000),
                 contentDescription = "Weather",
                 tint = MaterialTheme.colorScheme.primary
-            )*/
+            )
         }
 
         if (showPopup) {

c30542150ceaf60ade82a67dc4574f855a06d3e3
Noah <niteigen@uio.no>
2025-04-10 11:03:46 +0200

    Pirking

--- app/src/main/java/in2000/team42/data/frost/FrostDatasource.kt
+++ app/src/main/java/in2000/team42/data/frost/FrostDatasource.kt
@@ -21,8 +21,6 @@ import kotlinx.coroutines.withContext
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 
-// TODO: Filtrere ut nullverdier og gi komplett data til HomeViewModel
-
 class FrostDatasource() {
     private val TAG = "FrostDatasource" // LogCat tag for denne klassen
     private val CLIENTID = "5fa50311-61ee-4aa0-8f29-2262c21212e5"

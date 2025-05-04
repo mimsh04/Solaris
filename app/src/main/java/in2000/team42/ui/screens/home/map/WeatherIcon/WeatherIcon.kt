75ae6726c07b9175f09d7e646e82cc22b06ddddd
Noah <niteigen@uio.no>
2025-05-05 00:53:19 +0200

    Flyttet litt rundt på popup screen slik at den ikke vises over AdresseFelt

--- app/src/main/java/in2000/team42/ui/screens/home/map/WeatherIcon/WeatherIcon.kt
+++ app/src/main/java/in2000/team42/ui/screens/home/map/WeatherIcon/WeatherIcon.kt
@@ -28,6 +28,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -35,6 +36,7 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Popup
 import com.composables.core.Icon
@@ -49,8 +51,8 @@ fun WeatherIconButton(
     modifier: Modifier = Modifier,
     viewModel: HomeViewModel
 ) {
-    var showPopup by remember { mutableStateOf(false) }
-    var showImageInfo by remember { mutableStateOf(false) }
+    var showPopup by rememberSaveable { mutableStateOf(false) }
+    var showImageInfo by rememberSaveable { mutableStateOf(false) }
     val apiData by viewModel.apiDataFlow.collectAsState()
     val weatherData = apiData.weatherData
 
@@ -72,13 +74,19 @@ fun WeatherIconButton(
         else -> R.drawable.ic_sol
     }
 
+    val backgroundSize = 50.dp
+    val iconSize = 44.dp
+
     Box(modifier = modifier) {
         IconButton(
-            onClick = { showPopup = true }
+            onClick = { showPopup = true },
+            modifier = Modifier
+                .size(backgroundSize)
+                .offset(x = 10.dp)
         ) {
             Box(
                 modifier = Modifier
-                    .size(60.dp) // Størrelse for bakgrunn til ikon
+                    .size(backgroundSize) // Størrelse for bakgrunn til ikon
                     .background(
                         color = MaterialTheme.colorScheme.background,
                         shape = CircleShape
@@ -89,81 +97,121 @@ fun WeatherIconButton(
                     painter = painterResource(id = iconResource),
                     contentDescription = "Weather",
                     modifier = Modifier
-                        .size(40.dp) // Størrelse for ikon
+                        .size(iconSize) // Størrelse for ikon
                         .padding(4.dp), // Padding gjør at ikon vil alltid være mindre enn bakgrunnen
                     colorFilter = if (iconResource == R.drawable.ic_unknown_weather) {
-                        ColorFilter.tint(MaterialTheme.colorScheme.onSurface) // Apply gray tint only to ic_unknown_weather
+                        ColorFilter.tint(MaterialTheme.colorScheme.onSurface)
                     } else {
-                        null // No color filter for other drawables
+                        null
                     }
                 )
             }
         }
+    }
 
-        if (showPopup) {
-            Popup(
-                onDismissRequest = { showPopup = false },
-                alignment = Alignment.Center
+    if (showPopup) {
+        Popup(
+            onDismissRequest = { showPopup = false },
+            alignment = Alignment.TopStart,
+            offset = IntOffset(0, 200)
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .width(250.dp),
+                shape = RoundedCornerShape(8.dp),
+                shadowElevation = 4.dp,
+                color = MaterialTheme.colorScheme.surface
             ) {
-                Surface(
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .width(250.dp),
-                    shape = RoundedCornerShape(8.dp),
-                    shadowElevation = 4.dp,
-                    color = MaterialTheme.colorScheme.surface
+                Column(
+                    modifier = Modifier.padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     Column(
-                        modifier = Modifier.padding(16.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally
+                        modifier = Modifier.fillMaxWidth(),
                     ) {
-                        Column(
-                            modifier = Modifier.fillMaxWidth(),
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(bottom = 8.dp)
                         ) {
-                            Box(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(bottom = 8.dp)
-                            ) {
-                                Text(
-                                    text = "Gjennomsnittlig vær for måneden",
-                                    style = MaterialTheme.typography.titleMedium,
-                                    textAlign = TextAlign.Center,
-                                    modifier = Modifier.fillMaxWidth()
-                                )
-                                IconButton(
-                                    onClick = { showImageInfo = !showImageInfo },
-                                    modifier = Modifier
-                                        .size(32.dp)
-                                        .align(Alignment.CenterEnd)
-                                        .offset(x = ((10).dp)) // Offset fra midten til høyre siden
-                                ) {
-                                    Icon(
-                                        imageVector = Icons.Filled.Info,
-                                        contentDescription = "Bildeinfo",
-                                        tint = MaterialTheme.colorScheme.onSurfaceVariant,
-                                        modifier = Modifier.size(24.dp)
-                                    )
-                                }
-                            }
-                        }
-
-                        if (weatherData.isEmpty() || latestWeather == null) {
                             Text(
-                                text = "Værdata ukjent.\n\nKunne ikke hente værdata fra dette området.\n\nPrøv å velge en annen adresse.",
+                                text = "Gjennomsnittlig vær for måneden",
+                                style = MaterialTheme.typography.titleMedium,
                                 textAlign = TextAlign.Center,
                                 modifier = Modifier.fillMaxWidth()
                             )
-                        } else {
-                            Text("Temperatur: ${latestWeather.temp}")
-                            Text("Snø: ${latestWeather.snow}")
-                            Text("Skydekke: ${latestWeather.cloud}")
+                            IconButton(
+                                onClick = { showImageInfo = !showImageInfo },
+                                modifier = Modifier
+                                    .size(32.dp)
+                                    .align(Alignment.CenterEnd)
+                                    .offset(x = ((10).dp)) // Offset fra midten til høyre siden
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Filled.Info,
+                                    contentDescription = "Bildeinfo",
+                                    tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                                    modifier = Modifier.size(24.dp)
+                                )
+                            }
                         }
+                    }
 
-                        Spacer(modifier = Modifier.height(16.dp))
+                    if (weatherData.isEmpty() || latestWeather == null) {
+                        Text(
+                            text = "Værdata ukjent.\n\nKunne ikke hente værdata fra dette området.\n\nPrøv å velge en annen adresse.",
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    } else {
+                        Text("Temperatur: ${latestWeather.temp}")
+                        Text("Snø: ${latestWeather.snow}")
+                        Text("Skydekke: ${latestWeather.cloud}")
+                    }
 
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Button(
+                        onClick = { showPopup = false },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.primary
+                        )
+                    ) {
+                        Text("Lukk")
+                    }
+                }
+            }
+        }
+    }
+
+    if (showImageInfo) {
+        Popup(
+            onDismissRequest = { showImageInfo = false },
+            alignment = Alignment.Center
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.onBackground.copy(alpha = 0.4f)),
+                contentAlignment = Alignment.Center
+            ) {
+                Surface(
+                    modifier = Modifier
+                        .fillMaxWidth(0.8f)
+                        .wrapContentHeight()
+                        .padding(16.dp),
+                    shape = RoundedCornerShape(12.dp),
+                    shadowElevation = 8.dp,
+                    color = MaterialTheme.colorScheme.surface
+                ) {
+                    Column(
+                        modifier = Modifier.padding(24.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        CreditsScreen()
                         Button(
-                            onClick = { showPopup = false },
+                            onClick = { showImageInfo = false },
                             colors = ButtonDefaults.buttonColors(
                                 containerColor = MaterialTheme.colorScheme.primary
                             )
@@ -174,61 +222,17 @@ fun WeatherIconButton(
                 }
             }
         }
-
-        if (showImageInfo) {
-            Popup(
-                onDismissRequest = { showImageInfo = false },
-                alignment = Alignment.Center
-            ) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(MaterialTheme.colorScheme.onBackground.copy(alpha = 0.4f)),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Surface(
-                        modifier = Modifier
-                            .fillMaxWidth(0.8f)
-                            .wrapContentHeight()
-                            .padding(16.dp),
-                        shape = RoundedCornerShape(12.dp),
-                        shadowElevation = 8.dp,
-                        color = MaterialTheme.colorScheme.surface
-                    ) {
-                        Column(
-                            modifier = Modifier.padding(24.dp),
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
-                            CreditsScreen()
-                            Button(
-                                onClick = { showImageInfo = false },
-                                colors = ButtonDefaults.buttonColors(
-                                    containerColor = MaterialTheme.colorScheme.primary
-                                )
-                            ) {
-                                Text("Lukk")
-                            }
-                        }
-                    }
-                }
-            }
-        }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun WeatherIconButtonPreview() {
-    // Mock DisplayWeather for preview
     val mockWeather = DisplayWeather(
         month = "Jan 2025",
         temp = "5.0°C",
         snow = "0.0mm",
         cloud = "30.0%"
     )
-
-
-    MaterialTheme {
-        WeatherIconButton(viewModel = HomeViewModel())
-    }
 }

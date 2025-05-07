07eb72b8055f53495fc4c83c655cfe4a6f68d135
Mats Herzeth Christiansen <matshc@uio.no>
2025-05-07 14:32:45 +0200

    pyntet på produksjonsvisning

--- app/src/main/java/in2000/team42/ui/screens/home/bottomSheet/Produksjon.kt
+++ app/src/main/java/in2000/team42/ui/screens/home/bottomSheet/Produksjon.kt
@@ -1,27 +1,22 @@
 package in2000.team42.ui.screens.home.bottomSheet
 
 
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import in2000.team42.data.pgvis.model.KwhMonthlyResponse
 import in2000.team42.data.produksjonKalkulering.calculateWithCoverage
 import in2000.team42.ui.screens.home.ApiData
 
@@ -61,7 +56,16 @@ fun Produksjon(apiData: ApiData) {
                 Column  (
                     horizontalAlignment = Alignment.CenterHorizontally
                 ){
-                    Text("Årlig produksjon: ${getYearlyProduction(apiData).toInt()} kWh",
+
+                    Text(" ⚡ Produksjon: ${getYearlyProduction(apiData).toInt()} kWh",
+                        color = MaterialTheme.colorScheme.onBackground,
+                        style = MaterialTheme.typography.titleLarge,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Spacer(modifier = Modifier.size(6.dp))
+
+                    Text(
+                        "💰 Anslått besparelse: ${(getYearlyProduction(apiData) * 0.5).toInt()} kr",
                         color = MaterialTheme.colorScheme.onBackground,
                         style = MaterialTheme.typography.titleLarge,
                         fontWeight = FontWeight.Bold

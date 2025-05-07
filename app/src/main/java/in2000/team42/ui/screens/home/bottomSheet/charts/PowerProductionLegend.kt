4ba0386a47619998cbb79d3171e4b2d25ab50216
Sindre Denby <sidenby1@gmail.com>
2025-05-08 00:32:56 +0200

    Fjernet imports

--- app/src/main/java/in2000/team42/ui/screens/home/bottomSheet/charts/PowerProductionLegend.kt
+++ app/src/main/java/in2000/team42/ui/screens/home/bottomSheet/charts/PowerProductionLegend.kt
@@ -1,20 +1,13 @@
 package in2000.team42.ui.screens.home.bottomSheet.charts
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp

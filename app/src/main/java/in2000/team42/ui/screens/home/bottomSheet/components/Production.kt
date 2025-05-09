aba6142f520b18f0440df56dcac9ce100807f8db
Mats Herzeth Christiansen <matshc@uio.no>
2025-05-09 16:48:19 +0200

    la til en import

--- app/src/main/java/in2000/team42/ui/screens/home/bottomSheet/components/Production.kt
+++ app/src/main/java/in2000/team42/ui/screens/home/bottomSheet/components/Production.kt
@@ -3,6 +3,7 @@ package in2000.team42.ui.screens.home.bottomSheet.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer

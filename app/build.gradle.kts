a7697392b27198e72420f8d82a238d8a5cec417e
Sindre Denby <sidenby1@gmail.com>
2025-05-15 12:15:55 +0200

    endret min sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "in2000.team42"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
